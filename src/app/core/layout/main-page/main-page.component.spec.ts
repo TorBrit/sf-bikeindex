0fa8d99b57bc3a39b2e26937fd8eca652ba0ebe9
Torben Knulst <torben.knulst@hybrit.org>
2023-11-09 11:35:21 +0100

    Fix broken unit tests, add missing component tests

--- src/app/core/layout/main-page/main-page.component.spec.ts
+++ src/app/core/layout/main-page/main-page.component.spec.ts
@@ -1,16 +1,45 @@
+import { BehaviorSubject, of } from 'rxjs';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { MainPageComponent } from './main-page.component';
+import { QueryParams } from 'src/app/api/models/query-params';
 
 describe('MainPageComponent', () => {
   let component: MainPageComponent;
   let fixture: ComponentFixture<MainPageComponent>;
 
+  const mockParams = new BehaviorSubject<QueryParams>({
+    location: {
+      city: 'Delft',
+    },
+    pageNumber: 1,
+    pageSize: 10,
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [MainPageComponent],
+      providers: [
+        HttpClient,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({
+              search: mockParams.value.location.city,
+              page: mockParams.value.pageNumber,
+            }),
+          },
+        }
+      ],
+      imports: [
+        HttpClientTestingModule
+      ],
     });
     fixture = TestBed.createComponent(MainPageComponent);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -19,7 +48,13 @@ describe('MainPageComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // TODO it:
-  // should load data
-  // should show error notification if data could not be loaded
+  it('should prefill initial data from queryparams in relevant components', () => {
+    const ac: ActivatedRoute = fixture.debugElement.injector.get(ActivatedRoute);
+    ac.queryParams.subscribe((v) => {
+      expect(v).toEqual({ search: 'Delft', page: 1 });
+    });
+  });
+
+  // TODO it's:
+  // should update search input and query params when updating
 });
